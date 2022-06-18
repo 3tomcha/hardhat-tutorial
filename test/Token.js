0fd16a776252caf5d0709da3b556f0e5ed491183
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-06-18 13:41:39 +0900

    succeeded to transfer test

--- test/Token.js
+++ test/Token.js
@@ -26,4 +26,41 @@ describe("Token Contract", function() {
             expect(await hardhatToken.balanceOf(owner.address)).to.equal(await hardhatToken.totalSupply());
         });
     });
+
+    describe("Transactions", function() {
+        // transfer
+        // amountがない場合にエラーになる
+        // 送るとバランスが減る
+        // 送られるとバランスが増える
+        it("Transferすると送られた側のバランスが増える", async function() {
+            await hardhatToken.transfer(addr1.address, 100);
+            expect(await hardhatToken.balanceOf(addr1.address)).to.equal(100);
+        });
+        it("Transferすると送った側のバランスが減る", async function() {
+            await hardhatToken.transfer(addr1.address, 100);
+            expect(await hardhatToken.balanceOf(addr1.address)).to.equal(100);
+            await hardhatToken.connect(addr1).transfer(addr2.address, 100);
+            expect(await hardhatToken.balanceOf(addr1.address)).to.equal(0);
+            expect(await hardhatToken.balanceOf(addr2.address)).to.equal(100);
+        });
+        it("amountがない場合にエラーになる", async function() {
+            await expect(hardhatToken.connect(addr1).transfer(addr2.address, 100)).to.be.revertedWith("Not enough tokens");
+            // expect(await hardhatToken.balanceOf(addr1.address)).to.be.revertedWith("Not enough tokens");
+        });
+        it("複数回transferを行うと更新される", async function() {
+            const initialOwnerBalance = await hardhatToken.balanceOf(owner.address);
+            await hardhatToken.transfer(addr1.address, 100);
+            await hardhatToken.transfer(addr2.address, 50);
+
+            const finalOwnerBalance = await hardhatToken.balanceOf(owner.address);
+
+            expect(initialOwnerBalance.sub(150)).to.equal(finalOwnerBalance);
+
+            const addr1Balance = await hardhatToken.balanceOf(addr1.address);
+            const addr2Balance = await hardhatToken.balanceOf(addr2.address);
+
+            expect(addr1Balance).to.equal(100);
+            expect(addr2Balance).to.equal(50);
+        });
+    });
 });
