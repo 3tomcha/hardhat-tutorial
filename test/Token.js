fbdb2a8382a55b76b93a8396e5d400bb9c603931
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-06-18 13:21:29 +0900

    succeeded to deploy test

--- test/Token.js
+++ test/Token.js
@@ -1,24 +1,29 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Token Contract", function() {
-    it("Deployment should assign the total supply of tokens to the owner", async function() {
-        const [owner] = await ethers.getSigners();
-        const Token = await ethers.getContractFactory("Token");
-        const hardhatToken = await Token.deploy();
-        const ownerBalance = await hardhatToken.balanceOf(owner.address);
-        expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
-    })
-})
 
-describe("Transactions", function() {
-    it("should transfer tokens between accounts", async function() {
-        const [owner, addr1, addr2] = await ethers.getSigners();
-        const Token = await ethers.getContractFactory("Token");
-        const hardhatToken = await Token.deploy();
-        await hardhatToken.transfer(addr1.address, 50);
-        expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
-        await hardhatToken.connect(addr1).transfer(addr2.address, 50);
-        expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);
-    })
-})
+describe("Token Contract", function() {
+
+    let Token;
+    let hardhatToken;
+    let owner;
+    let addr1;
+    let addr2;
+    let addrs;
+
+    beforeEach(async function() {
+        Token = await ethers.getContractFactory("Token");
+        [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+        hardhatToken = await Token.deploy();
+    });
+
+    describe("Deployment", function() {
+        it("Should set the right owner", async function() {
+            expect(await hardhatToken.owner()).to.equal(owner.address);
+        });
+        it("ownerが全てのtokenを持つ", async function() {
+            expect(await hardhatToken.balanceOf(owner.address)).to.equal(await hardhatToken.totalSupply());
+        });
+    });
+});
